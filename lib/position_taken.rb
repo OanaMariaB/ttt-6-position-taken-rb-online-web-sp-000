da0e0832d7f3623bb60d0e6d92cef46a59afd542
Oana Berentan <oana.berentan@yahoo.com>
2019-04-10 21:11:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 def position_taken?(board, index)
-  if board[index_number] == " "
+  if (board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]) == " "
     false
 end
